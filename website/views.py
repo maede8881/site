50b5b738543d686d2ed905c7b7dee1f3891ec643
maede8881 <asadimaedeh90@gmail.com>
2025-09-22 16:45:22 +0330

    added general templates folder

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ def index_views(request):
 
 
 def about_views(request):
-    return render(request,'website/about.html')
+    return render(request,'website/about.html') 
 
 
 def contact_views(request):
