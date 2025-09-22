c5b75506e460f7e76790c690df2aece851bef871
maede8881 <asadimaedeh90@gmail.com>
2025-09-22 14:01:24 +0330

    created render html files

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path('',index_views),
+    path('index',index_views),
     path('about',about_views),
     path('contact',contact_views)
 ]    
--- website/views.py
+++ website/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_views(request):
-    return HttpResponse('<h1>home page</h1>')
+    return render(request,'website/index.html')
+
 
 def about_views(request):
-    return HttpResponse('<h1>about page</h1>')
+    return render(request,'website/about.html')
 
-def about_views(request):
-    return HttpResponse('<h1>about page</h1>')
 
 def contact_views(request):
-    return HttpResponse('<h1>contact page</h1>')
+    return render (request,'website/contact.html')
