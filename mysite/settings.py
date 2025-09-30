21a7b65093e1bfac384fbfe95650017aabb3ff19
maede8881 <asadimaedeh90@gmail.com>
2025-09-30 15:21:05 +0330

    aded static directories and urls in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,9 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_DIR = [
+    BASE_DIR /"statics"
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
